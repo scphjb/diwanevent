4e57c41db5ddf1c6fd55cb417c0aff720798ff70
amir <scphjb@gmail.com>
2026-05-31 16:18:12 +0100

    fix: change toggle switch container from div to label to fix click issue

--- dashboard/src/pages/SettingsPage.jsx
+++ dashboard/src/pages/SettingsPage.jsx
@@ -328,7 +328,7 @@ const SettingsPage = () => {
                            </div>
                            <span className="font-bold text-white">{feat.label}</span>
                          </div>
-                         <div className="relative inline-flex items-center cursor-pointer">
+                         <label className="relative inline-flex items-center cursor-pointer">
                             <input 
                               type="checkbox" 
                               className="sr-only peer"
@@ -336,7 +336,7 @@ const SettingsPage = () => {
                               onChange={(e) => handleChange(feat.id, e.target.checked)}
                             />
                             <div className="w-14 h-7 bg-white/10 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-brand-primary"></div>
-                         </div>
+                         </label>
                       </div>
                     ))}
                   </div>
